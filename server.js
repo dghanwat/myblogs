9aa03ea07aeb03c31dce6fd69f2c1d4e3f03cf18
[a506826] Dhananjay Ghanwat <dhananjay.ghanwat@atos.net>
2017-01-02 12:36:48 +0530

    added dynamic og

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var PORT = Number(process.env.PORT || 5000);
 
 var compression = require('compression');
 var express = require('express');
+var ejs = require('ejs');
+var fs = require('fs');
 var app = express();
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/www');
+app.set('view engine', 'html');
 
 app.use(compression());
 
@@ -16,7 +21,26 @@ app.get('/home', function (req, res) {
   res.sendFile(__dirname + '/www/index.html');
 });
 app.get('/blog/*', function (req, res) {
-  res.sendFile(__dirname + '/www/index.html');
+  //res.sendFile(__dirname + '/www/index.html');
+
+  var urlFileName = req.url.substring(req.url.lastIndexOf("/")+1,req.url.length);
+  var indexData = JSON.parse(fs.readFileSync('./www/js/data/index.json', 'utf8'));
+  var arrayFound = indexData.filter(function(item) {
+    	return item.fileName == urlFileName;
+	});
+  if(arrayFound.length == 0){
+	   res.render('index', {
+	    ogUrl: "https://outofmymemory.herokuapp.com/home",
+	    ogTitle: "OutOfMyMemory",
+	    ogDescription:"We are a respected blog with niche content. The articles provide useful best practices, tutorials, articles, code samples and snippets on latest technologies such as Core Java, AngularJS , Design Patterns, Ionic Framework, Scala etc."
+	  });
+  } else {
+ 	res.render('index', {
+	    ogUrl: "https://outofmymemory.herokuapp.com"+req.url,
+	    ogTitle: "OutOfMyMemory:"+arrayFound[0].title,
+	    ogDescription:arrayFound[0].shortText
+	  });
+  }
 });
 
 var server = app.listen(PORT, function () {
